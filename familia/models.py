a4f3aaaf744a529885b04893474385d5f0ffa31b
cmateluna <cmateluna@fundacioncchc.cl>
2022-11-02 14:17:21 -0300

    v2.0

--- familia/models.py
+++ familia/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 
+
 class Persona(models.Model):
-    nombre = models.CharField(max_lenght=100)
+    nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField()
--- familia/views.py
+++ familia/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from familia.models import Persona
 
 # Create your views here.
+
+def index(request):
+    personas = Persona.objects.all()
+    template = loader.get_template('familia/lista_familiares.html')
+    context = {
+        'personas': personas,
+    }
+    return HttpResponse(template.render(context, request))
