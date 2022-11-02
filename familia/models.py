e6ac2e1d67362bc29fac32c7b1beec8f6daba283
cmateluna <cmateluna@fundacioncchc.cl>
2022-11-02 10:35:30 -0300

    readme.md actualizado

---
+++ familia/models.py
@@ -0,0 +1,10 @@
+from concurrent.futures.process import _MAX_WINDOWS_WORKERS
+from django.db import models
+
+# Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_lenght=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
